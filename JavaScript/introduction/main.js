02ac20b5b308974094c291849b19e3796c2eedbd
baalamboox <jmg201098@gmail.com>
2024-05-09 13:57:34 -0600

    JS introduction completed

---
+++ JavaScript/introduction/main.js
@@ -0,0 +1,6 @@
+// This is an external JS file.
+
+window.alert("Are you OK?");
+console.log("You're in the head!");
+console.log("Hi there!");
+document.getElementById("paragraph").innerHTML = "My name is: Guillermo.";
