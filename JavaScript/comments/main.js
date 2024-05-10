6601ebd540e28401c54a0d388e36823a6ae7e7e7
baalamboox <jmg201098@gmail.com>
2024-05-10 10:33:31 -0600

    JS comments completed

---
+++ JavaScript/comments/main.js
@@ -0,0 +1,28 @@
+// This is an inline comment.
+
+/**
+ * This a multi line comments
+ * Only use for documentation
+*/
+
+
+// This statement works to show a simple message on the page.
+document.querySelector('body').innerHTML = "Hi there!";
+
+
+// console.log("Happy hacking!");
+
+/*
+function greatings() {
+    let name = "Guillermo";
+    console.log(`Hello ${name}!`);
+}
+
+greatings();
+*/
+
+/**
+ * When can I use a comment?
+ * For say important something about our code.
+ * For invalid a simple line statement or a multi line statements.
+ */
