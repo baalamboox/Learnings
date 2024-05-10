1c4c5dad51c78d0dee6f6f5b83f4ff428e7c070f
baalamboox <jmg201098@gmail.com>
2024-05-09 22:26:15 -0600

    JS syntax completed

---
+++ JavaScript/syntax/main.js
@@ -0,0 +1,39 @@
+// Variable declarations before ES6.
+
+// This is a old way for create a new variable.
+names = "Guillermo";
+
+console.log(names);
+
+// This is an old version for create a new variable.
+var age = 17;
+
+console.log(age);
+
+// Variable declarations after ES6
+
+
+let sex = "Male";
+
+console.log(sex);
+
+
+const tall = 2;
+
+console.log(tall);
+
+// var is a keyword, this means that can't used.
+
+
+// This is a inline comment.
+
+/**
+ * This is a block comment o multi line.
+ */
+
+let a = 10;
+
+// The variable names is recomended to call with a specific name.
+
+let myFavouriteSong = "Snowman - Sia";
+console.log(myFavouriteSong);
