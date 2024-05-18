27971d24b7998e2951c8953b6c9757036b8e50f8
baalamboox <jmg201098@gmail.com>
2024-05-18 00:00:54 -0600

    JS if-else completed

---
+++ JavaScript/if-else/main.js
@@ -0,0 +1,17 @@
+document.querySelector("title").innerHTML = "JS | If else";
+
+/**
+ * What's a if?
+ * Conditional statements are used to perform different actions based on different conditions.
+ */
+
+let user = "baalambooxs";
+
+if(user === "baalamboox") {
+    // All code here
+    console.log("Success!");
+} else if(user == "baalambooxs") {
+    console.log("user semicorrect!");
+} else {
+    console.log("Icorrect user!");
+}
