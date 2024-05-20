d7788f085a3cd5bfc88735c2fa0d5ee91443047e
baalamboox <jmg201098@gmail.com>
2024-05-20 15:29:10 -0600

    JS strict mode completed

---
+++ JavaScript/stric-mode/main.js
@@ -0,0 +1,16 @@
+"use strict"; // Allows to give a good programing practice.
+
+document.querySelector("title").innerHTML = "JS | Strict mode";
+
+let myName;
+
+myName = "Guillermo";
+
+// let myName = "Guillerm";
+
+hi();
+
+function hi() {
+    h = 10;
+    console.log("hola!");
+}
